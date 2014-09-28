627ff8bdabb5577e52436cc78aa88266f38846d5
Brady Mitchell <bradym@users.noreply.github.com>
2014-09-28 14:19:43 -0700

    Switch to single quotes instead of double, add config and gem spec files

---
+++ watcher.gemspec
@@ -0,0 +1,8 @@
+Gem::Specification.new do |s|
+  s.name  = "dir-watcher"
+  s.files = ["lib/watcher.rb"]
+  s.version = 0.1
+  s.summary = "Watch a directory and execute a command when files are added, modified or removed."
+  s.author  = "Brady Mitchell"
+  s.homepage = "https://github.com/bradym/dir-watcher"
+end
--- watcher.rb
+++ watcher.rb
@@ -9,58 +9,57 @@ require 'listen'
 require 'yaml'
 require 'trollop'
 
-trap("SIGINT") {
+trap('SIGINT') {
   exit!
 }
 
 opts = Trollop::options do
-  opt :config, "The config file to use", :default => "#{Dir.home}/.watcher.yaml"
+  opt :config, 'The config file to use', :default => "#{Dir.home}/.watcher.yaml"
 end
-
 
 if File.exist?(opts[:config])
   config = YAML.load_file(opts[:config])
 else
-  abort("No config file specified, or file does not exist.")
+  abort('No config file specified, or file does not exist.')
 end
 
-def validateJob(job)
+def validate_job(job)
   # Check that a directory was specified
-  if job["directory"].nil?
-    abort("Directory must be specified.")
+  if job['directory'].nil?
+    abort('Directory must be specified.')
   end
 
   # Make sure the specified directory exists
-  unless File.directory?(job["directory"])
-    abort("Specified directory does not exist.")
+  unless File.directory?(job['directory'])
+    abort('Specified directory does not exist.')
   end
 end
 
-config["jobs"].each do |key, job|
+config['jobs'].each do |key, job|
 
-  validateJob(job)
+  validate_job(job)
 
   options = {
-      :filter                   => job["filter"].nil? ? nil : Regexp.new(job["filter"]),
-      :ignore                   => job["ignore"].nil? ? nil : Regexp.new(job["ignore"]),
-      :relative_paths           => job["relative_paths"].nil? ? false : job["relative_paths"]
+      :filter                   => job['filter'].nil? ? nil : Regexp.new(job['filter']),
+      :ignore                   => job['ignore'].nil? ? nil : Regexp.new(job['ignore']),
+      :relative_paths           => job['relative_paths'].nil? ? false : job['relative_paths']
   }.reject{|key, value| value.nil?}
 
 
-  Listen.to(job["directory"], options) do |modified, added, removed|
+  Listen.to(job['directory'], options) do |modified, added, removed|
 
     event = {:modified => modified, :added => added, :removed => removed}.reject{|key,value| value.empty?}
 
     action = event.keys.first.to_s
     file   = event.values.first
 
-    if job["command"].is_a? Hash
-      unless job["command"][action].nil?
-        command = job["command"][action]
+    if job['command'].is_a? Hash
+      unless job['command'][action].nil?
+        command = job['command'][action]
       end
     else
-      if job["command"].is_a? String
-        command = job["command"]
+      if job['command'].is_a? String
+        command = job['command']
       end
     end
 
